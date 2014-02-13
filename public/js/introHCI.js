68f90545a7cb5f692b94b9787dea1aee6eacf3f5
vagrant <vagrant@precise32.(none)>
2014-02-13 22:11:16 +0000

    ajax lab complete

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -61,6 +61,13 @@ function randomizeColors(e) {
 	$.get("/palette",newFunction2);
 }
 
-function newFunction2(){
+function newFunction2(result){
 
+	console.log(result);
+ var colors=result['colors']['hex'];
+$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);	
 }
